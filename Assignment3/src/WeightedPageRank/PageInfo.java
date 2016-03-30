cf5f9af644c94ac79afe06e8e8eb2e095844bb8f
Min Guo <xmguomin@gmail.com>
2016-03-29 23:06:57 -0400

    OutLink score works

--- Assignment3/src/WeightedPageRank/PageInfo.java
+++ Assignment3/src/WeightedPageRank/PageInfo.java
@@ -1,14 +1,18 @@
 package WeightedPageRank;
 
+import java.util.Map;
+import java.util.HashMap;
 
 public class PageInfo {
     String pageName;
     double baseScore;
+    Map<String, Double> outLinkScore = new HashMap<>();
 
 
-    public PageInfo(String name, double baseScore) {
+    public PageInfo(String name, double baseScore, Map<String, Double> outLinkScore) {
         this.pageName = name;
         this.baseScore = baseScore;
+        this.outLinkScore = outLinkScore;
     }
 
     public double getBaseScore() {
@@ -17,6 +21,10 @@ public class PageInfo {
 
     public void setNomalizedBaseScore(double baseScore) {
         this.baseScore = baseScore;
+    }
+
+    public void setOutLinkScore(String outLink, double score) {
+        outLinkScore.put(outLink, score);
     }
 
 
--- Assignment3/src/WeightedPageRank/PageRank.java
+++ Assignment3/src/WeightedPageRank/PageRank.java
@@ -1,15 +1,12 @@
 package WeightedPageRank;
 
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import java.io.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import java.io.File;
 import java.util.*;
-import java.io.IOException;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class PageRank {
     static List<PageInfo> pageInfos = new ArrayList<>();
@@ -27,29 +24,77 @@ public class PageRank {
         return  (unNomalizedbaseScore/totalScore);
     }
 
+    static Map<String, Double> calcOutLinkScore(Document doc) {
+        int totalScore = 0;
+        Map<String, Double> totalLinks = new HashMap<>();
+        Map<String, Integer> tagScopeLinks = new HashMap<>();
+        String[] tagList = {"H1", "H2", "H3", "H4", "em", "b"};
+        Elements links = doc.select("a[href]");
+        for (Element link : links) {
+            String linkString = link.attr("href");
+            if (totalLinks.containsKey(linkString)) {
+                totalLinks.put(linkString, totalLinks.get(linkString) + 1);
+            } else {
+                totalLinks.put(linkString, 1.0);
+            }
+        }
+
+        for (String tag:tagList) {
+                Elements section = doc.select(tag);
+                section = section.select("a").unwrap();
+                String sectionHtml = section.attr("href");
+                if (!sectionHtml.equals("")) {
+                    if (tagScopeLinks.containsKey(sectionHtml)) {
+                        tagScopeLinks.put(sectionHtml, tagScopeLinks.get(sectionHtml) + 1);
+                    } else {
+                        tagScopeLinks.put(sectionHtml, 1);
+                    }
+                }
+        }
+
+        for (Map.Entry<String, Double> entry : totalLinks.entrySet()) {
+            double score;
+            if (tagScopeLinks.containsKey(entry.getKey())) {
+                score = tagScopeLinks.get(entry.getKey()) * 2 + (entry.getValue() - tagScopeLinks.get(entry.getKey()));
+                totalLinks.put(entry.getKey(), score);
+            }
+        }
+
+        for (Map.Entry<String, Double> entry : totalLinks.entrySet()) {
+            totalScore += entry.getValue();
+        }
+
+        for (Map.Entry<String, Double> entry : totalLinks.entrySet()) {
+            double nomalizedScore;
+            nomalizedScore = entry.getValue()/totalScore;
+            totalLinks.put(entry.getKey(), nomalizedScore);
+        }
+        return totalLinks;
+    }
+
     static void readPages(String folderPath) throws IOException{
         File folder = new File(folderPath);
         File[] listOfFiles = folder.listFiles();
-        double totalScore = 0;
+        double totalBaseScore = 0;
 
         for (File file: listOfFiles) {
             double unNomalizedbaseScore;
+            Map<String, Double> outLinksScore;
             if (file.isFile() && (getExtension(file.getName()).equals("html"))) {
                 Document doc = Jsoup.parse(file, null);
+                outLinksScore = calcOutLinkScore(doc);
                 String text = doc.body().text();
                 String[] textSplit = text.split("\\s+");
                 unNomalizedbaseScore = Math.log(textSplit.length) / Math.log(2);
-                System.out.println(textSplit.length);
-                totalScore += unNomalizedbaseScore;
-                PageInfo pageInfo = new PageInfo(file.getName(), unNomalizedbaseScore);
+                totalBaseScore += unNomalizedbaseScore;
+                PageInfo pageInfo = new PageInfo(file.getName(), unNomalizedbaseScore, outLinksScore);
                 pageInfos.add(pageInfo);
             }
         }
 
         for (PageInfo pageInfo:pageInfos) {
-            pageInfo.setNomalizedBaseScore(calcBaseScore(pageInfo.getBaseScore(), totalScore));
+            pageInfo.setNomalizedBaseScore(calcBaseScore(pageInfo.getBaseScore(), totalBaseScore));
         }
-
     }
 
     public static void main(String[] args) throws IOException{
